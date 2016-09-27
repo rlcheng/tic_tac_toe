e9149572e85a631c9eca289ce938133e9f6e3d27
Richard Cheng <rlcheng.7@gmail.com>
2016-09-26 23:08:04 -0700

    Move player order choice from Game to Launch

--- launch.rb
+++ launch.rb
@@ -1,8 +1,36 @@
 require_relative 'game'
 require_relative 'user_interface'
 
-ui = UserInterface.new
-game = Game.new ui
-while game.game_over == false do
-  game.loop
+def select_order
+  choice = @UI.get_choice
+  @player = Player.new(choice, @UI)
+  @UI.message("You are Player #{@player.marker}")
+
+  if choice == 1
+    @cpu = Cpu.new(2)
+    @current_player = @player
+  else
+    @cpu = Cpu.new(1)
+    @current_player = @cpu
+  end
+  @UI.message("CPU is Player #{@cpu.marker}")
+
+  
 end
+
+def init
+  @UI = UserInterface.new
+
+  select_order
+
+  @game = Game.new(@UI, @player, @cpu, @current_player)
+end
+
+def launch
+  while @game.game_over == false do
+    @game.loop
+  end
+end
+
+init
+launch
